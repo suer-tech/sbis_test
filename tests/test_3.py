91701cde233538f810e112d9627d7d958038b957
suer-tech <suer2505@gmail.com>
2024-03-07 17:33:41 +0300

    Add test_download_plugin to test_3.py

--- tests/test_3.py
+++ tests/test_3.py
@@ -1,3 +1,4 @@
+from pages.download_sbis_page import DownloadSbisPage
 from pages.home_page import HomePage
 
 
@@ -5,3 +6,16 @@ def test_go_to_download_sbis(browser):
     url = 'https://sbis.ru/'
     home_page = HomePage(browser, url)
     home_page.open_download_sbis_page()
+
+
+def test_download_plugin(browser):
+    url = 'https://sbis.ru/'
+    home_page = HomePage(browser, url)
+    home_page.open_download_sbis_page()
+    download_sbis_page = DownloadSbisPage(browser, browser.current_url)
+    download_sbis_page.go_to_sbis_plugin()
+    download_sbis_page.open_page_for_windows()
+
+
+
+

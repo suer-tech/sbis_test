2ad0339ab6d9f94975c310a1913df32b05ab28b5
suer-tech <suer2505@gmail.com>
2024-03-07 17:34:43 +0300

    Add WINDOWS to locators.py

--- locators.py
+++ locators.py
@@ -7,7 +7,7 @@ class Locator:
     TENZOR_BANNER = (By.XPATH, "//a[@href='https://tensor.ru/']")
     REGION = (By.XPATH, "//span[@class='sbis_ru-Region-Chooser__text sbis_ru-link']")
     PARTNERS_LIST = (By.CSS_SELECTOR, "div[name='itemsContainer']")
-    CITY = (By.ID,"city-id-2")
+    CITY = (By.ID, "city-id-2")
 
     POWER_IN_PEOPLE_BLOCK = (By.XPATH, "//div[@class='tensor_ru-Index__block4-bg']")
     POWER_IN_PEOPLE_BLOCK_DETAILS = (By.CSS_SELECTOR, "a.tensor_ru-link[href='/about']")
@@ -16,6 +16,7 @@ class Locator:
     IMAGE = (By.XPATH, "//div[@class='tensor_ru-About__block3-image-wrapper']")
 
     DOWNLOAD_SBIS = (By.XPATH, "//a[text()='Скачать локальные версии']")
-
+    SBIS_PLUGIN = (By.XPATH, "//div[text()='СБИС Плагин']")
+    WINDOWS = (By.XPATH, "//span[text()='Windows']")
 
 

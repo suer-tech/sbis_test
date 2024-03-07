35240bfe21f8d96f813a4a425978f33de93b7610
suer-tech <suer2505@gmail.com>
2024-03-07 17:34:15 +0300

    Add open_page_for_windows to download_sbis_page.py

--- pages/download_sbis_page.py
+++ pages/download_sbis_page.py
@@ -1,6 +1,14 @@
+from locators import Locator
 from pages.base_page import BasePage
 
 
 class DownloadSbisPage(BasePage):
+    def go_to_sbis_plugin(self):
+        sbis_plugin = self.find_element(Locator.SBIS_PLUGIN)
+        sbis_plugin.click()
+
+    def open_page_for_windows(self):
+        page_for_windows = self.find_element(Locator.WINDOWS)
+        page_for_windows.click()
 
 
